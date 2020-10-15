f75eecc153ac62bd1983da0e1c622cb4a50f2894
Manish <maneesh_patel@outlook.com>
2020-10-15 17:48:53 +0100

    Chages made in base class on 15/10/2020

--- TestExcersice/src/main/java/Base/BaseClass.java
+++ TestExcersice/src/main/java/Base/BaseClass.java
@@ -20,7 +20,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 
-
+/*
+ * This is The Base Class 
+ */
 
 public class BaseClass {
 	
